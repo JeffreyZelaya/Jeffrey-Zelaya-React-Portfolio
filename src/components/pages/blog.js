20e7b74641f43486ae4473a4255e46862c5d01b5
Jeffrey Zelaya <jeffreyz.dev@gmail.com>
2020-02-24 16:49:32 -0700

    added link to about me on blog page

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,9 +1,12 @@
 import React from "react"
-
+import {Link} from "react-router-dom"
 export default function () {
     return(
         <div>
-            Blog
+            <h2>Blog</h2>
+            <div>
+                <Link to="/about-me">Read more about Jeffrey Zelaya</Link>
+            </div>
         </div>
     )
 }
