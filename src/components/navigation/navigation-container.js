7dca3bbd575f73d50d46eccfc061d93d66828424
Jeffrey Zelaya <jeffreyz.dev@gmail.com>
2020-02-24 15:59:14 -0700

    added a router for the hoe and about

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react"
 
-export default class NavigationComponents extends Component {
+export default class NavigationContainer extends Component {
     constructor (){
         super ()
     }
