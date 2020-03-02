b34185ba16ed90841a58b5ec46e784c8a5fb3a60
Jeffrey Zelaya <jeffreyz.dev@gmail.com>
2020-03-02 14:04:01 -0700

    Guide to Select Dropdowns in React

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -45,7 +45,7 @@ export default class PortfolioForm extends Component {
         ).then(response => {
             console.log("response", response)
         }).catch(error => {
-            (console.log("portfolio form handleSUbmit error", error))
+            (console.log("portfolio form handleSubmit error", error))
         })
         
         event.preventDefault()
@@ -59,38 +59,40 @@ export default class PortfolioForm extends Component {
                 <form onSubmit={this.handleSubmit}>
                 <div>
                         <input 
-                        type="text"
-                        name="name"
-                        placeholder= "Portfolio Item Name"
-                        value={this.state.name}
-                        onChange={this.handleChange}
+                            type="text"
+                            name="name"
+                            placeholder= "Portfolio Item Name"
+                            value={this.state.name}
+                            onChange={this.handleChange}
                         />
 
                         <input 
-                        type="text"
-                        name="url"
-                        placeholder= "URL"
-                        value={this.state.url}
-                        onChange={this.handleChange}
+                            type="text"
+                            name="url"
+                            placeholder= "URL"
+                            value={this.state.url}
+                            onChange={this.handleChange}
                         />
                     </div>
 
                     <div>
                         <input 
-                        type="text"
-                        name="position"
-                        placeholder= "Position"
-                        value={this.state.position}
-                        onChange={this.handleChange}
+                            type="text"
+                            name="position"
+                            placeholder= "Position"
+                            value={this.state.position}
+                            onChange={this.handleChange}
                         />
 
-                        <input 
-                        type="text"
-                        name="category"
-                        placeholder= "Category"
-                        value={this.state.category}
-                        onChange={this.handleChange}
-                        />
+                        <select 
+                            name="category"
+                            value={this.state.category}
+                            onChange={this.handleChange}
+                        >
+                            <option value="eCommerce">eCommerce</option>
+                            <option value="Scheduling">Scheduling</option>
+                            <option value="Enterprise">Enterprise</option>
+                        </select>
                     </div>
 
                     <div>
