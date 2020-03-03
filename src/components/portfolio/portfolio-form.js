6e5d2c4e2f213977347aeb7a8a66481609569ac3
Jeffrey Zelaya <jeffreyz.dev@gmail.com>
2020-03-03 10:45:13 -0700

    How to Implement Custom Select and Textarea Styles

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -105,9 +105,7 @@ export default class PortfolioForm extends Component {
     }
     render() {
         return (
-            <div>
-                <h1>PortfolioForm</h1>
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit} className="portfolio-form-wrapper">
                     <div>
                         <input
                             type="text"
@@ -133,6 +131,7 @@ export default class PortfolioForm extends Component {
                             onChange={this.handleChange}
                         />
                         <select
+                            className="select-element"
                             name="category"
                             value={this.state.category}
                             onChange={this.handleChange}
@@ -175,7 +174,6 @@ export default class PortfolioForm extends Component {
                         <button type="submit">Save</button>
                     </div>
                 </form>
-            </div>
         )
     }
 }
