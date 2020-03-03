6b0594a6d5e90890baff286b336b6155de3c0507
Jeffrey Zelaya <jeffreyz.dev@gmail.com>
2020-03-03 16:25:04 -0700

    Making Dynamic Axios API Queries in React

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -14,7 +14,10 @@ export default class PortfolioForm extends Component {
             url: "",
             thumb_image: "",
             banner_image: "",
-            logo: ""
+            logo: "",
+            editMode: false,
+            apiUrl: 'https://jeffreyzelaya.devcamp.space/portfolio/portfolio_items',
+            apiAction: 'post'
         };
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
@@ -51,9 +54,9 @@ export default class PortfolioForm extends Component {
                 category: category || "eCommerce",
                 position: position || "",
                 url: url || "",
-                thumb_image: "",
-                banner_image: "",
-                logo: ""
+                editMode: true,
+                apiUrl: `https://jeffreyzelaya.devcamp.space/portfolio/portfolio_items/${id}`,
+                apiAction: "patch"
             })            
         }
     }
@@ -110,29 +113,31 @@ export default class PortfolioForm extends Component {
         });
     }
     handleSubmit(event) {
-        axios.post(
-            'https://jeffreyzelaya.devcamp.space/portfolio/portfolio_items', 
-            this.buildForm(), 
-            { withCredentials: true}
-        ).then(response => {
-            this.props.handleSuccessfulFormSubmission(response.data.portfolio_item)
-            this.setState({
-                name: "",
-                description: "",
-                category: "eCommerce",
-                position: "",
-                url: "",
-                thumb_image: "",
-                banner_image: "",
-                logo: ""
-            });
-            [this.thumbRef, this.bannerRef, this.logoRef].forEach(ref => {
-                ref.current.dropzone.removeAllFiles();
-            })
-        }).catch(error => {
-            console.log("portfolio for handleSubmit error", error);
+        axios({
+            method: this.state.apiAction,
+            url: this.state.apiUrl,
+            data: this.buildForm(),
+            withCredentials: true
         })
-            event.preventDefault();
+            .then(response => {
+                this.props.handleSuccessfulFormSubmission(response.data.portfolio_item)
+                this.setState({
+                    name: "",
+                    description: "",
+                    category: "eCommerce",
+                    position: "",
+                    url: "",
+                    thumb_image: "",
+                    banner_image: "",
+                    logo: ""
+                });
+                [this.thumbRef, this.bannerRef, this.logoRef].forEach(ref => {
+                    ref.current.dropzone.removeAllFiles();
+                })
+            }).catch(error => {
+                console.log("portfolio for handleSubmit error", error);
+            })
+                event.preventDefault();
     }
     render() {
         return (
