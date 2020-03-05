1a6af7f933311c52e6f3061e6b91ba5c29ce3d8b
Jeffrey Zelaya <jeffreyz.dev@gmail.com>
2020-03-05 15:13:23 -0700

    Converting the Functional Blog Component into a Class Component

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from "react-router-dom"
 
-export default function() {
-    return (
-        <div>
-            <h2>Blog</h2>
-
+export default class Blog extends Component {
+    constructor() {
+        super()
+    }
+    
+    render() {
+        return (
             <div>
-                <Link to="/about-me">Read more aboutthe one and only Jeffrey Zelaya</Link>
+                <h2>Blog</h2>
+
+                <div>
+                    <Link to="/about-me">Read more aboutthe one and only Jeffrey Zelaya</Link>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    
 }
