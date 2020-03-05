c88625212dc9eb0880879e4d45e43cb0fbe4fc2a
Jeffrey Zelaya <jeffreyz.dev@gmail.com>
2020-03-05 13:18:48 -0700

    Populating Image Thumbnail Conditionals and Styles

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -56,7 +56,10 @@ export default class PortfolioForm extends Component {
                 url: url || "",
                 editMode: true,
                 apiUrl: `https://jeffreyzelaya.devcamp.space/portfolio/portfolio_items/${id}`,
-                apiAction: "patch"
+                apiAction: "patch",
+                thumb_image: thumb_image_url || "",
+                banner_image: banner_image_url || "",
+                logo: logo_url || ""
             })            
         }
     }
@@ -194,35 +197,50 @@ export default class PortfolioForm extends Component {
                             onChange={this.handleChange}
                         />
                     </div>
-                    <div className="image-uploaders">
-                        <DropzoneComponent
-                            ref={this.thumbRef}
-                            config={this.componentConfig()}
-                            djsConfig={this.djsConfig()}
-                            eventHandlers={this.handleThumbDrop()}
-                         >
-                             <div className="dz-message">Thumbnail</div>
-                        </DropzoneComponent>
-
-                        <DropzoneComponent
-                            ref={this.bannerRef}
-                            config={this.componentConfig()}
-                            djsConfig={this.djsConfig()}
-                            eventHandlers={this.handleBannerDrop()}
-                        >
-                            <div className="dz-message">Banner</div>
-                        </DropzoneComponent>
-
-                        <DropzoneComponent
-                            ref={this.logoRef}
-                            config={this.componentConfig()}
-                            djsConfig={this.djsConfig()}
-                            eventHandlers={this.handleLogoDrop()}
-                        >
-                            <div className="dz-message">Logo</div>
-                        </DropzoneComponent> 
-                    </div>
-                    <div>
+            <div className="image-uploaders">
+              {this.state.thumb_image && this.state.editMode ? (
+                <div className="portfolio-manager-image-wrapper">
+                  <img src={this.state.thumb_image} />
+                </div>
+              ) : (
+                <DropzoneComponent
+                    ref={this.thumbRef}
+                    config={this.componentConfig()}
+                    djsConfig={this.djsConfig()}
+                    eventHandlers={this.handleThumbDrop()}
+                >
+                    <div className="dz-message">Thumbnail</div>
+                </DropzoneComponent>
+              )}
+              {this.state.banner_image && this.state.editMode ? (
+                <div className="portfolio-manager-image-wrapper">
+                  <img src={this.state.banner_image} />
+                </div>
+              ) : (
+                <DropzoneComponent
+                    ref={this.bannerRef}
+                    config={this.componentConfig()}
+                    djsConfig={this.djsConfig()}
+                    eventHandlers={this.handleBannerDrop()}
+                >
+                    <div className="dz-message">Banner</div>
+                </DropzoneComponent>
+              )}
+              {this.state.logo && this.state.editMode ? (
+                <div className="portfolio-manager-image-wrapper">
+                  <img src={this.state.logo} />
+                </div>
+              ) : (
+                <DropzoneComponent
+                    ref={this.logoRef}
+                    config={this.componentConfig()}
+                    djsConfig={this.djsConfig()}
+                    eventHandlers={this.handleLogoDrop()}
+                >
+                    <div className="dz-message">Logo</div>
+                </DropzoneComponent>
+              )}
+          
                         <button className="btn" type="submit">Save</button>
                     </div>
                 </form>
