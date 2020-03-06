4f9f0a0d141b523d96744257295aefc97505a9b4
Jeffrey Zelaya <jeffreyz.dev@gmail.com>
2020-03-06 14:59:28 -0700

    Implementing an Animated Data Loading Icon in React

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Link } from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import axios from 'axios'
 
 import BlogItem from "../blog/blog-item"
@@ -11,7 +12,8 @@ export default class Blog extends Component {
         this.state = {
             blogItems : [],
             totalCount: 0,
-            currentPage: 0
+            currentPage: 0,
+            isLoading: true
         }
 
         this.getBlogItems = this.getBlogItems.bind(this)
@@ -39,7 +41,8 @@ export default class Blog extends Component {
         .then(response => {
             this.setState({
                 blogItems: response.data.portfolio_blogs,
-                totalCount: response.data.meta.total_records
+                totalCount: response.data.meta.total_records,
+                isLoading: false
             })
         })
         .catch(error => {
@@ -61,8 +64,15 @@ export default class Blog extends Component {
                 <div className="content-container">
                     {blogRecords}
                 </div>
-            </div>
+                { this.state.isLoading ? ( 
+                    <div className="content-loader">
+                        <FontAwesomeIcon icon="yin-yang" spin/>
+                    </div>
+                ) : ( null
+                )}
+           </div> 
         )
+               
     }
     
 }
