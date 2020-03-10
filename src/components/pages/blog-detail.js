727e77a3ca241e273ab1832d2bbcdf67e6dbe03a
Jeffrey Zelaya <jeffreyz.dev@gmail.com>
2020-03-10 14:26:16 -0600

    Adding Click Handler to Render the BlogForm Component

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -2,6 +2,7 @@ import React, {Component} from "react"
 import axios from "axios"
 import ReactHtmlParser from "react-html-parser";
 
+import BlogForm from "../blog/blog-form"
 import BlogFeaturedImage from "../blog/blog-featured-image"
 
 export default class BlogDetail extends Component {
@@ -9,9 +10,17 @@ export default class BlogDetail extends Component {
         super(props)
         this.state = {
             currentID: this.props.match.params.slug,
-            blogItem: {}
+            blogItem: {},
+            editMode: false
         }
+
+        this.handleEditClick = this.handleEditClick.bind(this)
     }
+
+    handleEditClick() {
+        console.log("handle edit clicked");
+        this.setState({ editMode: true })
+      }
 
     getBlogItem() {
         axios.get(
@@ -38,16 +47,24 @@ export default class BlogDetail extends Component {
             blog_status
         } = this.state.blogItem
 
+        const contentManager =() => {
+            if (this.state.editMode) {
+              return <BlogForm/>
+            } else {
+                return (
+                    <div className="content-container">
+                        <h1 onClick={this.handleEditClick}>{title}</h1>
+
+                        <BlogFeaturedImage img={featured_image_url}/>
+
+                        <div className="content">{ReactHtmlParser(content)}</div>
+                    </div>
+                )
+            }
+          }
+
         return (
-        <div className="blog-container">
-            <div className="content-container">
-                <h1>{title}</h1>
-
-                <BlogFeaturedImage img={featured_image_url}/>
-
-                <div className="content">{ReactHtmlParser(content)}</div>
-            </div>
-        </div>
+        <div className="blog-container">{contentManager()}</div>
         )
     }
     }
